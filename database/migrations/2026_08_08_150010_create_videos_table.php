edc277b0243e02bb4a6a307b44afc5c0ac254028
AhmedRamadan33 <engabdelaaty3210@gmail.com>
2026-08-19 21:16:50 +0300

    edit videos Migration

--- database/migrations/2026_08_08_150010_create_videos_table.php
+++ database/migrations/2026_08_08_150010_create_videos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('lesson_id')->constrained()->cascadeOnDelete();
-            $table->string('vdocipher_video_id')->nullable()->after('lesson_id');
+            $table->string('vdocipher_video_id')->nullable();
             $table->string('status')->default('created'); 
             $table->string('title')->nullable();
             $table->unsignedInteger('length_seconds')->default(0);
